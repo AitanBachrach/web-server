755f514369abed819005565c0ac0abe3fb001107
Aitan Bachrach <bachrachar@gmail.com>
2022-12-19 16:30:42 -0500

    index changes

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config();
 
-const app = express
-const PORT = process.env || 5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.get('/endpoint-1', (req, res, next) =>{
-    res.status(200).json({
-        success:true,
-        data: {
-            message: 'hello from endpoint 1'
-        } 
-    })
+    res.writeHead(200);
+    res.end('Hello, World!');
 })
 
 app.get('/endpoint-2', (req, res, next) =>{
@@ -24,4 +20,4 @@ app.get('/endpoint-2', (req, res, next) =>{
 })
 
 
-app.listen(PORT, () => console.log('Server is running on this port ${PORT}'))
+app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`))
